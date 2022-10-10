6c189fd783af80ce47441892414f76a9cd08294e
@ahermansan <aherman@san.edu.pl>
2022-10-10 09:30:50 +0200

    10.10.2022

---
+++ QuizConsole/Obiekty/Pytanie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsole.Obiekty
+{
+    public class Pytanie
+    {
+        public int Id { get; set; }
+        public int Kategoria { get; set; }
+        public string Tresc { get; set; }
+        public string Odpowiedz_01 { get; set; }
+        public string Odpowiedz_02 { get; set; }
+        public string Odpowiedz_03 { get; set; }
+        public string Odpowiedz_04 { get; set; }
+    }
+}
+
+
+
+
+// 100, 200, 500, 1000, 5000
---
+++ QuizConsole/Program.cs
@@ -0,0 +1,9 @@
+﻿using QuizConsole.Obiekty;
+
+Console.WriteLine("WITAJ W QUIZIE WIEDZY");
+Console.WriteLine("Spróbuj odpowiedzieć na 5 pytań");
+Console.WriteLine("POWODZENIA !!!");
+Console.WriteLine();
+
+var pytanie_01 = new Pytanie();
+var pytanie_02 = new Pytanie();
